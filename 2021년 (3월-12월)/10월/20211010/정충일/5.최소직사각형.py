6ed4dbb5a62468515a0847f91d2cdd80c36e8f58
xowns4817 <xowns4817@naver.com>
2022-01-06 18:03:33 +0900

    BOJ_11404_플로이드 풀이

--- 2021년 (3월-12월)/10월/20211010/정충일/5.최소직사각형.py
+++
@@ -1,5 +0,0 @@
-def solution(sizes):
-    sizes=[sorted(i) for i in sizes]
-    maxWidth = [i[0] for i in sizes]
-    maxLength = [i[1] for i in sizes]
-    return max(maxWidth) * max(maxLength)
--- 2021년 (3월-12월)/12월/20211212/김재현/README.md
+++
@@ -1,8 +0,0 @@
-# 외판원 순회 2  
-tsp.py
-# 스토쿠  
-sdocku.py
-# n m  
-nm.py
-# 여행경로  
-trabl.py
